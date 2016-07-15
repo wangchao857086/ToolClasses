b79db9b2287a34208bc33390f20f5a81afc245df
wangchao857086 <wangchao@bushangban.com>
2016-07-15 15:33:50 +0800

    Initial Commit

---
+++ UnionOfIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UnionOfIOS
+//
+//  Created by 王超 on 16/7/15.
+//  Copyright © 2016年 Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UnionOfIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UnionOfIOS
+//
+//  Created by 王超 on 16/7/15.
+//  Copyright © 2016年 Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
